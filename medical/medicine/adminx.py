beb853e118852156f10cd8b36b61dfeb42e7014c
kk <705555262@qq.com>
2020-04-19 14:27:30 +0800

    完成药品模型设计

--- medical/medicine/adminx.py
+++ medical/medicine/adminx.py
@@ -1,12 +1,20 @@
 import xadmin
 from xadmin import views
 
-from medical.medicine.models import Category
+from medical.medicine.models import Category, Medicine
 
 class CategoryAdmin:
-    list_display = ['name', 'desc','icon', "medicine_nums", 'created_at', 'updated_at']
+    list_display = ['image_data', 'name', 'description', "medicine_nums", 'created_at', 'updated_at']
     list_filter = ['name', 'created_at']
-    search_fields = ['name', 'desc']
+    search_fields = ['name', 'description']
     ordering = ['-medicine_nums']
 
+
+class MedicineAdmin:
+    list_display = ['image_data', 'name', 'description', "normal_price", 'promotion_price', 'sn', 'created_at', 'status']
+    list_filter = ['name', 'description']
+    search_fields = ['name', 'description']
+    ordering = ['-created_at']
+
 xadmin.site.register(Category, CategoryAdmin)
+xadmin.site.register(Medicine, MedicineAdmin)
